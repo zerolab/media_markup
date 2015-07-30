91377aad2771ace93a61b9e04af1f3ea6e5118a4
Dan Braghis <dan@zerolab.org>
2015-07-30 14:29:05 +0100

    Initial commit

---
+++ themes/media-markup.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file media_markup/themes/media-markup.tpl.php
+ */
+
+if ($display) :
+?>
+<div id="media-markup-<?php print $id; ?>" class="media-markup">
+  <?php print $content; ?>
+</div>
+<?php endif; ?>
